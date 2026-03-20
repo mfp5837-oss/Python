7f5242b3cddf051011201a5baa52bce9d0c74a1a
mfp5837-oss <mfp5837@psu.edu>
2026-03-20 11:45:51 -0400

    updated

--- homework 1.py
+++ homework 1.py
@@ -1,4 +1,28 @@
-for line in open("LICENSE.txt", "r", encoding="utf-8"):
-    for word in line.split():
-            if word.lower() == "ing":
-                print(word)
+import nltk
+import nltk.corpus
+# The next line downloads all the example texts used in the NLTK book at https://www.nltk.org/book !
+# You can comment out the download line after the first time you do it.
+nltk.download('book')
+from nltk.book import *
+# The next line lets us do GET requests from remote URLs on the web:
+from urllib import request
+# The following import lines are for plotting interactive visualizations in Python
+import matplotlib
+import matplotlib.pyplot as plt
+# import tk ebb: Sorry this was INCORRECT! We need to distinguish tkinter from tensorkit.
+# These imports will let us make a simple tkinter user input / output interface:
+import tkinter as tk
+from tkinter import scrolledtext
+import io
+import sys
+
+plt.plot(range(10))
+plt.show()
+### See how these words are dispersed in NLTK text 1 (Moby Dick)
+words = ["whale", "sea", "ship", "captain"]
+nltk.draw.dispersion_plot(text1, words)
+plt.show()
+# Another dispersion plot written closer to the NLTK example:
+# Choose the text first (text 4 is Inaugural Addresses):
+text4.dispersion_plot(["citizens", "democracy", "freedom", "duties", "America"])
+plt.show()
