24639e5360805394cec11802902c24f1f789d1c4
mfp5837-oss <mfp5837@psu.edu>
2026-03-20 13:55:46 -0400

    homework 2

---
+++ homework2-mulan.py
@@ -0,0 +1,21 @@
+from urllib import request
+
+
+bookurl= "https://www.gutenberg.org/cache/epub/2081/pg2081.txt"
+response = request.urlopen(bookurl)
+br = response.read().decode('utf8')
+type(br)
+print(len(br))
+# make a variable
+howLong = len(br)
+# picture string version! 
+print(f"howLong = {howLong}")
+novelSlice = br[:500]
+print(f"novelSlice = {novelSlice}")
+
+splitEmUp = br.split()
+print(f"splitEmUp = {splitEmUp[-100:]}")
+
+for token in splitEmUp:
+    if token.endswith('ed'):
+        print(token)
