ce15394fd7f6c4676f1a83b9420dc59465c9d154
mfp5837-oss <mfp5837@psu.edu>
2026-03-20 14:06:58 -0400

    homework2 update

--- homework2-mulan.py
+++ homework2-mulan.py
@@ -2,6 +2,7 @@ from urllib import request
 
 
 bookurl= "https://www.gutenberg.org/cache/epub/2081/pg2081.txt"
+bookurl="https://www.gutenberg.org/cache/epub/2081/pg2081.txt"
 response = request.urlopen(bookurl)
 br = response.read().decode('utf8')
 type(br)
@@ -19,3 +20,4 @@ print(f"splitEmUp = {splitEmUp[-100:]}")
 for token in splitEmUp:
     if token.endswith('ed'):
         print(token)
+
